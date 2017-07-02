347ea3af70d71975c020fbafe71c4549947cc8df
LAPTOP-1FP83853\Peter <>
2017-07-02 17:25:43 -0400

    Updating mystepdefs

--- src/test/java/theVacationer/MyStepdefs.java
+++ src/test/java/theVacationer/MyStepdefs.java
@@ -5,6 +5,8 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Peter on 7/2/2017.
  */
@@ -22,5 +24,23 @@ public class MyStepdefs {
   @Then("^all the tests go green$")
   public void allTheTestsGoGreen() throws Throwable {
     //na
+  }
+
+  @Given("^A specific location$")
+  public void aSpecificLocation() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    assertEquals(1,1);
+  }
+
+  @When("^I chose to locate available restaurants$")
+  public void iChoseToLocateAvailableRestaurants() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    assertEquals(1,1);
+  }
+
+  @Then("^They're are (\\d+) available restaurants$")
+  public void theyReAreAvailableRestaurants(int arg0) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    assertEquals(1,1);
   }
 }
