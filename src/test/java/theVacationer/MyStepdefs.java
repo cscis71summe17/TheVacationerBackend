0de6bedaea9f8d988578ba4acd7138ac629ee85f
LAPTOP-1FP83853\Peter <>
2017-07-02 17:15:21 -0400

    Updating mystepdefs

---
+++ src/test/java/theVacationer/MyStepdefs.java
@@ -0,0 +1,26 @@
+package theVacationer;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by Peter on 7/2/2017.
+ */
+public class MyStepdefs {
+  @Given("^I have a scenario$")
+  public void iHaveAScenario() throws Throwable {
+    //na
+  }
+
+  @When("^I run the tests$")
+  public void iRunTheTests() throws Throwable {
+    //na
+  }
+
+  @Then("^all the tests go green$")
+  public void allTheTestsGoGreen() throws Throwable {
+    //na
+  }
+}
